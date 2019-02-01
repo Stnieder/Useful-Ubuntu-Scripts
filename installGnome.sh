70b6ce1e72df91f32cd3be55abcb62f8115759df
Stnieder <33837785+Stnieder@users.noreply.github.com>
2019-02-01 14:25:03 +0100

    Install gnome on ubuntu
    It adds the repository of the gnome3-team. Installs the following: gnome, gnome-shell, ubuntu-gnome-desktop, ubuntu-session and gdm3. Then you will be asked, if you want to remove unity and lightdm. Last, but not least it will reboot your pc to apply the changes.

---
+++ installGnome.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+function printQuestion {
+	echo -e "$1(${GREEN}y${NC}/${GREEN}n${NC})"
+}
+
+function printInColor {
+	echo -e "${$1} $2"
+}
+
+
+if [[ $(id -u) -ne 0 ]]; then
+	printInColor "RED" "Please run this script as sudo."
+	exit
+else
+	sudo apt-get dist-upgrade
+
+	sudo add-apt-repository ppa:gnome3-team/gnome3-staging
+	sudo add-apt-repository ppa:gnome3-team/gnome3
+
+	sudo apt update
+	
+	sudo apt install gnome gnome-shell ubuntu-gnome-desktop ubuntu-session gdm3
+	sudo apt autoremove
+
+	wait
+	
+	printQuestion "Do you want to remove untiy completely?"
+	read removeUnity
+
+	if [ "$removeUnity" == "y" ]
+	then
+		sudo apt purge unity-session unity
+		sudo apt autoremove
+	fi
+	
+	printQuestion "Do you want to remove 'lightdm'?"
+	read removeLightDM
+	
+	if [ "$removeLightDM" == "y" || "$removeLightDM" == "" ]
+	then
+		sudo apt-get purge lightdm
+		
+		sudo rm -rf /var/lib/lightdm-data
+		sudo rm -rf /etc/lightdm
+		
+		sudo apt autoremove
+	fi
+
+	printQuestion "To apply these changes, you have to restart your computer. Should I do that for you?"
+	read rebootNow
+
+	if [ "$rebootNow" == "y" ]
+	then
+		echo "Good luck with your new gnome system."
+		sleep 3
+		sudo reboot
+	else
+		echo "Good luck with your new gnome system."
+	fi
+fi
